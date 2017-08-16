050635bc8e5647e7052e87ffb9681615508ead60
Захаров Андрей <ZaharovA@tomskasu.ru>
2017-08-16 18:08:35 +0700

    Создание проекта.

---
+++ src/library-angular/webpack.config.js
@@ -0,0 +1,42 @@
+﻿/// <binding BeforeBuild='Run - Development' />
+"use strict";
+
+var path = require('path');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        polyfills: "./Scripts/polyfills.ts",
+        main: "./Scripts/app.ts"
+    },
+
+    resolve: {
+        extensions: ['', '.ts', '.js', '.json', '.css', '.html']
+    },
+
+    output: {
+        path: path.join(__dirname, "./wwwroot/"),
+        filename: "[name].js"
+    },
+
+    devServer: {
+        contentBase: ".",
+        host: "localhost",
+        port: 9000
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/, exclude: /node_modules/, loaders: [
+                    'ts-loader'
+                    , 'angular2-template-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
+};
